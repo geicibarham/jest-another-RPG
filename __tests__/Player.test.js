a4072b6db2ca16ac7967967e8f3748893eb9b628
geicibarham <geicianecosta43@gmail.com>
2022-02-28 14:21:38 -0500

    added health properties to test method

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,4 @@
-const Player = require('../lib/Player.js');
+
 const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
@@ -39,3 +39,36 @@ test('gets inverntory from player or return false', () => {
 
     expect(player.getInventory()).toEqual(false);
 });
+
+// create a test to get information about player's health
+
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+
+test("check if player is alive or not", () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
+
